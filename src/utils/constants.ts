a0e7e469551d05627d771cd8e079b6e485400d72
KewaiiGamer <miguelviegas1999@gmail.com>
2026-04-07 14:49:07 +0100

    feature: include kewaii in devs array

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -47,6 +47,10 @@ export interface Dev {
  * If you are fine with attribution but don't want the badge, add badge: false
  */
 export const Devs = /* #__PURE__*/ Object.freeze({
+    Kewaii: {
+        name: "Kewaii",
+        id: 797102656099319808n,
+    },
     Ven: {
         name: "V",
         id: 343383572805058560n
